f9f5e000fc92fcc869f78a612a680f1935d2e394
jcalabro <jamesrcalabro@gmail.com>
2024-01-24 02:06:51 -0500

    fix install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p $HOME/local/bin
-mkdir -p $HOME/.local/share/nvim/mason/bin/
+mkdir -p $HOME/.local/share/nvim/mason/packages/zls/bin
 
 zig build -Doptimize=ReleaseSafe
 cp zig-out/bin/zls $HOME/local/bin
-cp zig-out/bin/zls $HOME/.local/share/nvim/mason/bin/
+cp zig-out/bin/zls $HOME/.local/share/nvim/mason/packages/zls/bin
