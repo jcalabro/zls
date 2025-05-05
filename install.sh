017f1b05cac64c7d665eb0a05e509f2867343010
jcalabro <jamesrcalabro@gmail.com>
2025-05-04 20:06:20 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 mkdir -p $HOME/local/bin
 mkdir -p $HOME/.local/share/nvim/mason/packages/zls/bin
 
-zigup run 0.14.0-dev.2851+b074fb7dd build -Doptimize=ReleaseSafe
+zigup run 0.14.0 build -Doptimize=ReleaseSafe
 cp zig-out/bin/zls $HOME/local/bin
 cp zig-out/bin/zls $HOME/.local/share/nvim/mason/packages/zls/bin
